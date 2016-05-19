98374f071aacef29acedb55a3d87665369167932
jluzuria2001 <jluzuria2001@hotmail.com>
2016-05-19 14:45:56 +0200

    The script to lunch the publisher

--- mqtt-sn-tools/mqpub.sh
+++ mqtt-sn-tools/mqpub.sh
@@ -1,42 +1,38 @@
 #!/bin/bash
 #--------------------------------------------------------------------
-# interactive script to forwarding a MQTT message to a DTN host
-# to lunch it needs the -s or --start options
-# i.e. "./mqt2dtn.sh"
-#echo "./mqtt-sn-pub -h localhost -t "0000000000000000/ctrl" -p 20000 -m "1""
+# interactive script to publish MQTT message to a broker
+# to lunch it needs a "0" or "1" as options to publish a messages
+# to the motes that indicates that must start to publishing or just 
+# be subscribed to receive some messages.
+# i.e. "./mqpub.sh 0"
 
 
 #--------------------------------------------------------------------
 #--CONFIG--
 #host="localhost"
 host="192.168.1.48"
+#host="192.168.1.58"
 #host="2001:760:2e0b:1724::1"
 topicP="0000000000000000/msg"
 topicS="0000000000000000/ctrl"
 port="20000"
 
-#outfile="backpipe"
-#pid=$(cat pidfile)
-
-
-#(mosquitto_sub -h $host -t $topic > $outfile 2>/dev/null) & echo "$!" > pidfile
-#(bash mqtt-sn-sub -h $host -t $topic -p $port > $outfile 2>/dev/null) & echo "$!" > pidfile
 
 case "$1" in
 1)
 echo "PUBLISHING ->from<- the motes"
-./mqtt-sn-pub -h $host -t $topicS -p $port -m "1"
+./mqtt-sn-pub -h $host -t $topicS -p $port -m "4444"
 ;;
 0)
 COUNTER=0
 echo "PUBLISHING ->to<- the motes"
 while [ $COUNTER -lt 1800 ]; do
 	echo Publishing the message $COUNTER
-./mqtt-sn-pub -h $host -t $topicS -p $port -m "hello"
+./mqtt-sn-pub -h $host -t $topicS -p $port -m $COUNTER
 	sleep 1
 	let COUNTER=COUNTER+1
 done
-./mqtt-sn-pub -h $host -t $topicS -p $port -m "0"
+./mqtt-sn-pub -h $host -t $topicS -p $port -m "8888"
 ;;
 'help')
 echo "Usage: $0 [1 to PUB|0 to SUB]"
